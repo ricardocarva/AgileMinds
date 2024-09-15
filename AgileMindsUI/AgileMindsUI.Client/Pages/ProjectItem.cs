115365a972a02e5ccd67ed12d600a643107fbe79
Matt-Stre <97696979+Matt-Stre@users.noreply.github.com>
2024-09-15 13:25:02 -0400

    Add files via upload
    Added pages and class ProjectItem

---
+++ AgileMindsUI/AgileMindsUI.Client/Pages/ProjectItem.cs
@@ -0,0 +1,11 @@
+﻿namespace AgileMindsUI.Client.Pages
+{
+    public class ProjectItem
+    {
+        public int ID { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public String Person { get; set; }
+        public String Date {get; set;}
+    }
+}
